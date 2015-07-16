5ab901336d2d824030497fb9e0d0dac8d94ba73d
Nathan Townsend <nathan@nathantownsend.com>
2015-07-16 00:12:51 -0400

    Got action bar to show with three icons. Also added the custom theme to hide the action bar, as a proof of concept. The status bar is still showing. To show the action bar again just remove the custom theme settings in teh tiApp.xml file.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,40 @@
-function doClick(e) {
-    alert($.label.text);
-}
+$.index.activity.onCreateOptionsMenu = function(e) { 
+	var menu = e.menu; 
+	
+	var home = menu.add({ 
+		title : "Home", 
+		icon : Ti.App.Android.R.drawable.ic_home_white_24dp, 
+		showAsAction : Ti.Android.SHOW_AS_ACTION_IF_ROOM 
+	}); 
+	home.addEventListener("click", function(e) { 
+		alert("Home!");
+	}); 
+	
+	
+	var library = menu.add({
+		title: "Library",
+		icon: Ti.App.Android.R.drawable.ic_library_music_white_24dp,
+		showAsAction : Ti.Android.SHOW_AS_ACTION_IF_ROOM
+	});
+	library.addEventListener("click", function(e) { 
+		alert("Library!"); 
+	}); 
+	
+	
+	var favorites = menu.add({
+		title: "Library",
+		icon: Ti.App.Android.R.drawable.ic_favorite_border_white_24dp,
+		showAsAction : Ti.Android.SHOW_AS_ACTION_IF_ROOM
+	});
+	favorites.addEventListener("click", function(e) { 
+		alert("Favorites!"); 
+	}); 
+	
+	
+	
+};
+
+
 
 $.index.open();
+
